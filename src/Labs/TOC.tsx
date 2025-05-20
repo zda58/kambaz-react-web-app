a9d7aceb5e6bc151e54e368819f1347a40b7a83d
zda58 <107224274+zda58@users.noreply.github.com>
2025-05-19 23:11:25 -0400

    lab3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,19 +1,20 @@
 import Nav from "react-bootstrap/Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export default function TOC() {
+  const { pathname } = useLocation();
  return (
    <Nav variant="pills">
      <Nav.Item>
        <Nav.Link to="/Labs" as={Link}>Labs</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link to="/Labs/Lab1" as={Link}>Lab 1</Nav.Link>
+       <Nav.Link to="/Labs/Lab1" as={Link} active={pathname.includes("Lab1")}>Lab 1</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link to="/Labs/Lab2" as={Link} active>Lab 2</Nav.Link>
+       <Nav.Link to="/Labs/Lab2" as={Link} active={pathname.includes("Lab2")}>Lab 2</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link to="/Labs/Lab3" as={Link}>Lab 3</Nav.Link>
+       <Nav.Link to="/Labs/Lab3" as={Link} active={pathname.includes("Lab2")}>Lab 3</Nav.Link>
      </Nav.Item>
      <Nav.Item>
        <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
